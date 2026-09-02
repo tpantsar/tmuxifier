730afced242aa70cd18dc4d9a1895ae0e9b4c7ec
tpantsar <tomi.pantsar@gmail.com>
2026-09-02 11:34:51 +0300

    chore: edit session template

--- templates/session.sh
+++ templates/session.sh
@@ -1,11 +1,17 @@
-session_root "~/code/{{SESSION_NAME}}"
-window_root "~/code/{{SESSION_NAME}}"
+root="$HOME/code/{{SESSION_NAME}}"
+
+if [ ! -d "$root" ]; then
+  git clone ... "$root"
+fi
+
+session_root "$root"
+window_root "$root"
 
 if initialize_session "{{SESSION_NAME}}"; then
-  new_window "{{SESSION_NAME}}"
-  #run_cmd "git pull"
+  new_window "nvim"
 
-  #new_window "nvim"
+  new_window "{{SESSION_NAME}}"
+  run_cmd "git pull --rebase --autostash"
 
   select_window 1
 fi
